9a584b0bdcaee0a29895e799a8ce4654954ae8e6
vangarib <vangarib@ASIN18425113457>
2023-09-11 22:24:54 +0530

    11sep 10pm

--- src/main/java/com/dnb/DevConnector/DevConnectorApplication.java
+++ src/main/java/com/dnb/DevConnector/DevConnectorApplication.java
@@ -9,10 +9,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.dnb.DevConnector.dto.Profile;
 import com.dnb.DevConnector.dto.User;
 import com.dnb.DevConnector.exceptions.IdNotFoundException;
 import com.dnb.DevConnector.exceptions.InvalidEmailIdException;
 import com.dnb.DevConnector.exceptions.InvalidPasswordException;
+import com.dnb.DevConnector.exceptions.InvalidSkillsException;
+import com.dnb.DevConnector.service.ProfileService;
 import com.dnb.DevConnector.service.UserService;
 
 @SpringBootApplication
@@ -23,15 +26,23 @@ public class DevConnectorApplication {
 
 		UserService userService = applicationContext.getBean(UserService.class);
 
+		ProfileService profileService = applicationContext.getBean(ProfileService.class);
+
 		User user;
+
+		Profile profile;
 
 		try {
 			user = new User("Bhuvana", "bhuvana.1905j2@gmail.com", "Password@123");
+			profile = new Profile("Junior Developer", "DnB", "https://www.dnb.co.in/", "Hyderabad",
+					"HTML,CSS,JavaScript", "BhuvanaVangari", "Hi I'm Bhuvana", "www.twitter.com", "www.facebook.com",
+					null, "www.linkedin.com", "www.instagram.com");
+
 			Scanner sc = new Scanner(System.in);
 
 			while (true) {
 				System.out.println(
-						"Enter your choice\n1)Create user\n2)Get user by email ID\n3)Delete user by email ID\n4)Get all users\n5)Exit");
+						"Enter your choice\n1)Create user\n2)Get user by email ID\n3)Delete user by email ID\n4)Get all users\n5)Create profile\n6)Get profile by profileUUID\n7)Delete profile by profileUUID\n8)Get all profiles\n9)Exit");
 				int ch = sc.nextInt();
 
 				switch (ch) {
@@ -48,12 +59,28 @@ public class DevConnectorApplication {
 				case 3:
 					userService.deleteUserByEmailId("bhuvana.1905j2@gmail.com");
 					break;
-					
+
 				case 4:
-					userService.getAllUsers().forEach((name)->System.out.println(name));
+					userService.getAllUsers().forEach((name) -> System.out.println(name));
 					break;
-				
+
 				case 5:
+					profileService.createProfile(profile);
+					break;
+
+				case 6:
+					System.out.println(profileService.getProfileById("f212958f-cc8c-4073-887a-42e7d083a865")); 
+					break;
+					
+				case 7:
+					profileService.deleteProfileById("f212958f-cc8c-4073-887a-42e7d083a865");
+					break;
+					
+				case 8:
+					profileService.getAllProfiles().forEach((name)->System.out.println(name));
+					break;
+					
+				case 9:
 					System.exit(0);
 
 				}
@@ -70,8 +97,10 @@ public class DevConnectorApplication {
 		} catch (IdNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (InvalidSkillsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
 
 	}
 
