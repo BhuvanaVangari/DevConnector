d8818e178c0a5348b47ceeacf72debc372dd8df4
Bhuvana V <99163228+BhuvanaVangari@users.noreply.github.com>
2023-09-21 22:19:26 +0530

    RTE: dto not resolved

--- src/main/java/com/dnb/DevConnector/dto/SocialNetworkLinks.java
+++ src/main/java/com/dnb/DevConnector/dto/SocialNetworkLinks.java
@@ -1,8 +1,17 @@
 package com.dnb.DevConnector.dto;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
+@EqualsAndHashCode
+@NoArgsConstructor
+@ToString
 public class SocialNetworkLinks {
 	private String twitterURL;
 	private String facebookURL;
